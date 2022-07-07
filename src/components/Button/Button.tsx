6dddea8b1b855db0450d577d68fb8b0a41ef0654
JackStylesXYZ <107252780+JackStylesXYZ@users.noreply.github.com>
2022-07-07 16:59:50 +0100

    Base

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
-interface ButtonProps {
-  label: string;
+interface IButtonProps
+  extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  backgroundColor?: string;
+  color?: string;
+  size?: string;
 }
 
-export default function Button(props: ButtonProps) {
-  return <button>{props.label}</button>;
-}
+const Button: React.FunctionComponent<IButtonProps> = (props): JSX.Element => {
+  const { children, backgroundColor, color, style } = props;
+
+  let _style: React.CSSProperties = style || {};
+
+  /** Override defaults */
+  if (backgroundColor) _style.backgroundColor = backgroundColor;
+  if (color) _style.color = color;
+
+  return (
+    <button style={_style} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
